e89d1be80f1bcf4eac5bc92b28753056743e4bcb
Lukass9 <l_pisarek@o2.pl>
2023-11-13 05:39:36 +0100

    fix display photo for desktop

--- src/components/PhotoGallery/PhotoGallery.tsx
+++ src/components/PhotoGallery/PhotoGallery.tsx
@@ -1,17 +1,36 @@
+import { useEffect, useRef, useState } from "react";
 import "./photoGallery.scss";
 import { Images } from "src/types/type";
 
 export const PhotoGallery = ({ images }: { images: Images[] }) => {
+  const [activePhoto, setActivePhoto] = useState(0);
+  const imgWrappRef = useRef<HTMLDivElement | null>(null);
+  const imgWidth = -490;
+
+  useEffect(() => {
+    if (imgWrappRef.current) {
+      imgWrappRef.current.style.transform = `translateX(${
+        imgWidth * activePhoto
+      }px)`;
+    }
+  }, [activePhoto]);
   return (
     <div className='wrap-gallery'>
-      <div className='img-wrap'>
-        {images.map((image) => (
-          <img src={image.img} alt='' />
-        ))}
+      <div className='wrap-photo'>
+        <div className='img-wrap' ref={imgWrappRef}>
+          {images.map((image) => (
+            <img src={image.img} alt='' />
+          ))}
+        </div>
       </div>
       <div className='img-thumb'>
         {images.map((image) => (
-          <img src={image.thumb} alt='' />
+          <img
+            className={image.id == activePhoto ? "active" : ""}
+            src={image.thumb}
+            alt=''
+            onMouseEnter={() => setActivePhoto(image.id)}
+          />
         ))}
       </div>
     </div>
